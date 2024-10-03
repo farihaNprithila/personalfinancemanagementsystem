4ffa2b934302d1f683e18d81e574b945bb9eab94
fariha <farihanprithila@gmail.com>
2024-10-03 15:28:37 +0600

    basic project setup

---
+++ personalfinancemanagement/userservice/src/main/java/com/fariha/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fariha.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}

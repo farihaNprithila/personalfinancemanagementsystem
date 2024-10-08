b8b5176d6c468145b7dae04f13835512bdd77cc2
fariha <farihanprithila@gmail.com>
2024-10-08 15:03:23 +0600

    creating exception and repository

---
+++ personalfinancemanagement/accountservice/src/main/java/com/fariha/accountservice/common/utils/AppUtils.java
@@ -0,0 +1,5 @@
+package com.fariha.accountservice.common.utils;
+
+public final class AppUtils {
+    public static final String BASE_URL = "/api/v1/account-service";
+}
---
+++ personalfinancemanagement/accountservice/src/main/java/com/fariha/accountservice/common/utils/MessageUtils.java
@@ -0,0 +1,9 @@
+package com.fariha.accountservice.common.utils;
+
+public final class MessageUtils {
+    public static final String OPERATION_SUCCESSFUL = "Operation successful";
+    public static final String OPERATION_FAILED = "Operation failed";
+    public static final String OPERATION_CANCELLED = "Operation cancelled";
+    public static final String ACCOUNT_EXISTS = "Account already exists";
+    public static final String ACCOUNT_DOESNOT_EXISTS = "Account doesn't exists";
+}
